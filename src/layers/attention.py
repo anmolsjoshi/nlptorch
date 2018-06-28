12ea27a64adc35303b45cfb6f7217b0e35c913fb
anmolsjoshi <anmolsjoshi@gmail.com>
2018-06-28 07:09:53 -0700

    coattention layer

--- src/layers/attention.py
+++ src/layers/attention.py
@@ -1,4 +1,4 @@
-import torch as th
+import torch as T
 from torch import nn as nn
 from torch.nn import functional as F
 
@@ -13,8 +13,8 @@ class Similarity(nn.Module):
         #_, c_len, _ = y.shape
         #x = x.unsqueeze(2).repeat(1, 1, c_len, 1)
         #y = y.unsqueeze(1).repeat(1, q_len, 1, 1)
-        x_elementwise_y = th.mul(x, y)
-        matrix = th.cat([x, y, x_elementwise_y], 3)
+        x_elementwise_y = T.mul(x, y)
+        matrix = T.cat([x, y, x_elementwise_y], 3)
         S = self.wsim(matrix)
         S = S.squeeze()
         return S
@@ -26,11 +26,11 @@ class BasicAttention(nn.Module):
         super(BasicAttention, self).__init__()
     
     def forward(self, x, y):
-        e = th.bmm(x, y.transpose(1, 2))
+        e = T.bmm(x, y.transpose(1, 2))
         ai = F.softmax(e, dim=-1)
         
-        a = th.bmm(ai, y)
-        b = th.cat([x, a], dim=-1)
+        a = T.bmm(ai, y)
+        b = T.cat([x, a], dim=-1)
         
         out = self.dropout(b)
         
@@ -42,24 +42,39 @@ class BiAttention(nn.Module):
         super(BiAttention, self).__init__()
     
     def forward(self, S, x, y):
-        _, q_len, _ = x.shape
-        _, c_len, _ = y.shape
-        
-        context2query = th.bmm(F.softmax(S, dim=-1), x)
-        
-        b = F.softmax(th.max(S, 2)[0], dim=-1)
-        query2context = th.bmm(b.unsqueeze(1), y)
+        q_len = x.shape[1]
+        c_len = y.shape[1]
+
+        context2query = T.bmm(F.softmax(S, dim=-1), x)
+
+        b = F.softmax(T.max(S, 2)[0], dim=-1)
+        query2context = T.bmm(b.unsqueeze(1), y)
         query2context = query2context.repeat(1, c_len, 1)
         
         contextatten = x.mul(context2query)
         queryatten = y.mul(query2context)
         
-        return th.cat([x, context2query, contextatten, queryatten], 2)
+        return T.cat([x, context2query, contextatten, queryatten], 2)
     
     
 class CoAttention(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size):
         super(CoAttention, self).__init__()
+        self.Wqj = nn.Linear(2*hidden_size, 2*hidden_size)
+        self.c0 = nn.Parameter(T.rand(2*hidden_size,))
+        self.q0 = nn.Parameter(T.rand(2 * hidden_size, ))
     
-    def forward(self, x):
-        pass
+    def forward(self, q, c):
+        b, _, l = q.shape
+        qj = F.tanh(self.Wqj(q))
+        q_vec = self.c0.unsqueeze(0).expand(b, l).unsqueeze(1)
+        q = T.cat([qj, q_vec], dim=2)
+
+        c_vec = self.c0.unsqueeze(0).expand(b, l).unsqueeze(1)
+        c = T.cat([c, c_vec], dim=2)
+
+        L = T.bmm(q, c.transpose(1, 2))
+        alpha = F.softmax(L, dim=2)
+        beta = F.softmax(L, dim=1)
+
+        return alpha, beta
