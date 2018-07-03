ed50185c03b1bc8eae4f46b411bc24cb877d6bf9
Anmol Joshi <anmolsjoshi@gmail.com>
2018-07-03 14:04:53 -0700

    Split into 3 modules, WordEmbedding, CharacterEmbedding, CharacterConvEmbedding. To help with device placement i.e. nn.Embedding might perform better on CPU

--- src/layers/embedding.py
+++ src/layers/embedding.py
@@ -16,16 +16,31 @@ class WordEmbedding(nn.Module):
 
 class CharacterEmbedding(nn.Module):
 
-    def __init__(self, vocab_size, char_embed_dim, embedding_dim, kernel_sizes=[2, 3, 4 ,5], num_filters=50):
+    def __init__(self, vocab_size, embedding_dim, padding_idx, embeddings=None, trainable=True):
         super(CharacterEmbedding, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, char_embed_dim)
+        if embeddings is not None:
+            self.embedding = nn.Embedding.from_pretrained(embeddings=embeddings, freeze=trainable)
+        else:
+            self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx)
+
+    def forward(self, x):
+        return self.embedding(x)    
+    
+
+class CharacterConvEmbedding(nn.Module):
+
+    def __init__(self, char_embed_dim, embedding_dim, kernel_sizes=[2, 3, 4 ,5], num_filters=50, use_cuda=False):
+        super(CharacterConvEmbedding, self).__init__()
         self.conv = nn.ModuleList([nn.Conv2d(1, num_filters, (char_embed_dim, kernel)) for kernel in kernel_sizes])
         self.dropout = nn.Dropout(0.2)
         self.project = nn.Linear(len(kernel_sizes)*num_filters, embedding_dim)
+        self.use_cuda = use_cuda
+        
+        if self.use_cuda:
+            self.cuda()
 
     def forward(self, x):
         batch_size = x.size(0)
-        x = self.embedding(x)
         x = x.view(-1, x.size(-1), x.size(2)).unsqueeze(1)
 
         x = [F.relu(conv(x)).squeeze() for conv in self.conv]
