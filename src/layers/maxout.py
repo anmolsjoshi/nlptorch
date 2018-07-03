f6a496ad5f5a6b6f80be18d72d9773decfd7db23
Anmol Joshi <anmolsjoshi@gmail.com>
2018-07-03 14:06:13 -0700

    Updated Maxout module to include parameters (as needed), and changed original module name to MaxoutActivation

--- src/layers/maxout.py
+++ src/layers/maxout.py
@@ -1,9 +1,26 @@
 from torch import nn as nn
 
 class Maxout(nn.Module):
+    # https://github.com/pytorch/pytorch/issues/805
+    def __init__(self, d_in, d_out, pool_size):
+        super().__init__()
+        self.d_in, self.d_out, self.pool_size = d_in, d_out, pool_size
+        self.lin = nn.Linear(d_in, d_out * pool_size)
+        self.pool_size = pool_size
+
+    def forward(self, inputs):
+        shape = list(inputs.size())
+        shape[-1] = self.d_out
+        shape.append(self.pool_size)
+        max_dim = len(shape) - 1
+        out = self.lin(inputs)
+        m, _ = out.view(*shape).max(max_dim)
+        return m
+
+class MaxoutActivation(nn.Module):
     #https://github.com/pytorch/pytorch/issues/805
     def __init__(self, pool_size):
-        super().__init__()
+        super(MaxoutActivation, self).__init__()
         self._pool_size = pool_size
 
     def forward(self, x):
